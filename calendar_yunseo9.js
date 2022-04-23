6d3548f17565b591a9f3cfe3c727ac5491ff93a1
Kim-Yun-Seo <rlagrace410@gmail.com>
2022-04-23 11:40:57 +0900

    2월 한줄달력 완성

---
+++ calendar_yunseo9.js
@@ -0,0 +1,53 @@
+const monthsInfoData = [
+  { value: 1, name: 'January', endDate: 31 },
+  { value: 2, name: 'February', endDate: 28 },
+  { value: 3, name: 'March', endDate: 31 },
+  { value: 4, name: 'April', endDate: 30 },
+  { value: 5, name: 'May', endDate: 31 },
+  { value: 6, name: 'June', endDate: 30 },
+  { value: 7, name: 'July', endDate: 31 },
+  { value: 8, name: 'August', endDate: 31 },
+  { value: 9, name: 'September', endDate: 30 },
+  { value: 10, name: 'October', endDate: 31 },
+  { value: 11, name: 'November', endDate: 30 },
+  { value: 12, name: 'December', endDate: 31 },
+];
+const days = [
+  'sun',
+  'mon',
+  'tue',
+  'wed',
+  'thr',
+  'fri',
+  'sat'
+]
+const year = 2020;
+monthsInfoData[1].endDate = (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) ? 29 : 28;
+const calendarData = monthsInfoData.map((month) => {
+  const datesArray = Array(month.endDate).fill().map((_, index) => index + 1);
+  const firstDay = new Date(`${year}-${month.value}-1`).getDay();
+  const lastDay = new Date(`${year}-${month.value}-${month.endDate}`).getDay();
+  const frontEmptyArray = Array(firstDay).fill('');
+  const backEmptyArray = Array(6 - lastDay).fill('');
+
+  return [...frontEmptyArray, ...datesArray, ...backEmptyArray];
+})
+console.log('cal = ', calendarData );
+
+let dom = document.getElementById('saaa');
+console.log('dom = ', dom);
+let header = `<tr>`
+
+days.forEach((day) => {
+  header += `<th>${day}</th>`
+});
+header += `</tr>`
+header += `<tr>`
+calendarData[1].forEach((date) => {
+  header += `<td>${date}</td>`
+})
+header += `</tr>`
+
+
+
+dom.innerHTML = header;
